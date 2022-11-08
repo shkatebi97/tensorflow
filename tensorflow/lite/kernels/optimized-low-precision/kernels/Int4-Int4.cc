71784bfcb445a52c062440371bf24bd372471bef
S. Hossein Katebi <s.h.katebi97@gmail.com>
2022-11-08 11:06:41 +0330

    Optimized input packing for `I4-I4` method

--- tensorflow/lite/kernels/optimized-low-precision/kernels/Int4-Int4.cc
+++ tensorflow/lite/kernels/optimized-low-precision/kernels/Int4-Int4.cc
@@ -157,7 +157,6 @@ namespace LowPrecision{
                     if (is_multibatched){
                         int new_weights_length = ((int)shape.flatsize / 2);
                         temp = LowPrecision::allocate<int8_t>(new_weights_length);
-                        zero_vector(temp, new_weights_length);
                     }
                     int i , size = shape.flatsize;
                     asm volatile(
